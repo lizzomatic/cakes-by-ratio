5e9ae7ce1a52fbcf18fa68582c3d0580161f8f8d
picture picture <zz@picturepicture.local>
2026-01-20 17:52:49 -0800

    initial commit: cakes by ration django app

---
+++ cakes/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+import math
+
+def calculator(request):
+    recipe = None
+    error = None
+    eggs_input = ''
+    people_input = ''
+    
+    if request.method == 'POST':
+        try:
+            eggs = int(request.POST.get('eggs', 0))
+            people = int(request.POST.get('people', 0))
+            eggs_input = eggs
+            people_input = people
+            
+            if eggs <= 0 or people <= 0:
+                error = "Please enter positive numbers for eggs and people."
+            else:
+            
+                # Base recipe (for 1 egg)
+                base_recipe = {
+                    'banana': 3.0,
+                    'banana_max': 3.5,
+                    'eggs': 1,
+                    'baking_soda': 0.75,
+                    'flax_seeds': 1,
+                    'seeds_nuts_grains': 0.5,
+                    'cinnamon': 1,
+                    'ginger': 1,
+                    'vinegar': 2,
+                    'fresh fruit': 0.5,
+                }
+                
+                # Scale recipe by number of eggs & people
+                if (people > eggs):
+                    multiplier = eggs
+                else:
+                    multiplier = people
+                
+                recipe = {
+                    'banana': base_recipe['banana'] * multiplier,
+                    'banana_max': base_recipe['banana_max'] * multiplier,
+                    'eggs': base_recipe['eggs'] * multiplier,
+                    'baking_soda': base_recipe['baking_soda'] * multiplier,
+                    'flax_seeds': base_recipe['flax_seeds'] * multiplier,
+                    'seeds_nuts_grains': base_recipe['seeds_nuts_grains'] * multiplier,
+                    'cinnamon': base_recipe['cinnamon'] * multiplier,
+                    'ginger': base_recipe['ginger'] * multiplier,
+                    'vinegar': base_recipe['vinegar'] * multiplier,
+                    'fruit': base_recipe['fresh fruit'] * multiplier,
+                    'serves': eggs * 2,
+                    'cakes_made': eggs,
+                }
+                
+        except ValueError:
+            error = "Please enter valid numbers."
+    
+    return render(request, 'cakes/calculator.html', {
+        'recipe': recipe,
+        'error': error,
+        'eggs_input': eggs_input,
+        'people_input': people_input,
+    })
